0ec1f4081d281bc10b6ab2140fa161e2cfecfb54
Michael Duteau <mduteauii@gmail.com>
2025-06-27 22:08:11 -0400

    Initial GUI setup

---
+++ main.js
@@ -0,0 +1,36 @@
+import electron from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const { app, BrowserWindow } = electron;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    win.loadFile("gui/index.html");
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
